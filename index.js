dd4f792052bca36d58cbda5bdf6335cd98b24af1
alerner1 <alyssaplerner@gmail.com>
2021-01-31 20:30:19 -0500

    put in initial data

---
+++ index.js
@@ -0,0 +1,20 @@
+let artist = {
+  name: "Elvis Presley",
+  albums: [
+    {
+      title: "Elvis Presley",
+      year: 1956,
+      chartPosition: 1
+    },
+    {
+      title: "Elvis",
+      year: 1956,
+      chartPosition: 1
+    },
+    {
+      title: "Elvis' Christmas Album",
+      year: 1957,
+      chartPosition: 1
+    }
+  ]
+}
